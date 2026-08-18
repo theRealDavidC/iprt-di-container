246b7412bfcf1fd58937fa627637512bdde37ffc
DavidMmari <davidmmari98@gmail.com>
2026-08-18 13:03:14 +0300

    Phase 5: ReflectionUtils created with all four methods

---
+++ src/main/java/com/iprt/di/util/ReflectionUtils.java
@@ -0,0 +1,49 @@
+package com.iprt.di.util;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.reflect.Constructor;
+import com.iprt.di.annotations.Inject;
+import com.iprt.di.container.DIContainer;
+
+public class ReflectionUtils {
+ public static List<Field> getInjectableFields(Class<?> clazz) {
+  Field[] fields = clazz.getDeclaredFields();
+  List<Field> result = new ArrayList<>();
+  
+  for(Field classField : fields) {
+   if(classField.isAnnotationPresent(Inject.class)) {
+     result.add(classField);
+     }
+    }
+    return result;
+  }
+  
+ public static Constructor<?> getInjectableConstructor(Class<?> clazz) {
+  Constructor[] constructors = clazz.getDeclaredConstructors();
+  
+  for(Constructor classConstructor : constructors) {
+   if(classConstructor.isAnnotationPresent(Inject.class)) {
+     return classConstructor;
+    } 
+   }
+   return clazz.getDeclaredConstructor();
+    }
+    
+  public static Object createInstance(Class<?> clazz, Object... args) {
+   Constructor<?> constructor = getInjectableConstructor(clazz);
+   constructor.setAccessible(true);
+   return constructor.newInstance(args);
+   }
+   
+  public static void injectFields(Object instance, DIContainer container) {
+    List<Field> fields = getInjectableFields(instance.getClass());
+    
+    for(Field field : fields) {
+      field.setAccessible(true);
+      Object value = container.resolve(field.getType());
+      field.set(instance, value);
+      }
+     }
+}
